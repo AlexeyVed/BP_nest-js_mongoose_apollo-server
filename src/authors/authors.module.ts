c6b484c3eab2c9f1c4f6484b2bd9ec1eec1fb49d
Alexey <59867665+VedkalAlexey@users.noreply.github.com>
2021-09-07 18:46:53 +0300

    added mongodb, implemented simple models

--- src/authors/authors.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthorsResolver } from './authors.resolver';
-import { AuthorsService } from './authors.service';
-import { PostsService } from 'src/posts/post.service';
-
-@Module({
-  providers: [PostsService, AuthorsService, AuthorsResolver],
-})
-export class AuthorsModule {}
